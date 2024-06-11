1df176faba064e8b4e194a9f56895df26c13714e
kirillbarkhatov <k.s.barkhatov@gmail.com>
2024-06-12 02:35:45 +0300

    Выполнено доп задание 2

--- src/funcs_11_1.py
+++ src/funcs_11_1.py
@@ -11,6 +11,24 @@ def non_empty_truths(input_data:list) -> list:
     return result
 
 
+def my_map(f, xs: list) -> list:
+    """Упрощенный map"""
+    for i in range(len(xs)):
+        xs[i] = f(xs[i])
+    return xs
+
+
+def my_filter(f, xs):
+    """Упрощенный filter"""
+
+    return [x for x in xs if f(x)]
+
+
+def replicate_each(n, xs):
+    """Функция для каждого элемента итератора xs  выдает на выход по n копий этого элемента"""
+    return [i for x in xs for i in [x for j in range(n)]]
+
+
 
 if __name__ in "__main__":
     print(non_empty_truths([]))
@@ -23,3 +41,9 @@ if __name__ in "__main__":
     # []
     print(non_empty_truths([[0, 1, 2], [], [], [False, True, 42]]))
     # [[1, 2], [True, 42]]
+
+    print(list(my_map(lambda x: x + 2, [-1, 2, -3])))  # [1, 4, -1]
+    print(list(my_filter(lambda x: x % 2 == 1, range(10))))  # [1, 3, 5, 7, 9]
+    print(list(replicate_each(1, [1, 'a'])))  # [1, 'a']
+    print(list(replicate_each(3, [1, 'a'])))  # [1, 1, 1, 'a', 'a', 'a']
+    print(list(replicate_each(0, [1, 'a'])))  # []
